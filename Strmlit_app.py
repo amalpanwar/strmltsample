f56c788a71c7811b924db4207a53deae19000ed5
amalpanwar <panwar.amal1995@gmail.com>
2024-06-28 16:15:05 +0100

    Update Strmlit_app.py

--- Strmlit_app.py
+++ Strmlit_app.py
@@ -73,7 +73,11 @@ def create_radar_chart(df, players, id_column, title=None, max_values=None, padd
     ax.legend(loc='upper right', bbox_to_anchor=(0.1, 0.1))
     if title is not None:
         plt.suptitle(title)
-    plt.show()
+    
+    tmpfile = 'tmp.png'
+    plt.savefig(tmpfile)
+    st.image(tmpfile)
+    plt.close()
 # Streamlit app
 st.title('Player Performance Radar Chart')
 
@@ -82,7 +86,7 @@ players = st.selectbox('Select a player:', options=pivot_df.index.tolist())
 
 # Filter data for selected player
 #selected_data = pivot_df.loc[[player_selected]]
-
+st.subheader('Radar Chart for Selected Player and League Average')
 # Create radar chart for selected player
 create_radar_chart(pivot_df, [players, 'League Two Average'], id_column='Player', title='Radar Chart for Selected Players and League Average')
 
