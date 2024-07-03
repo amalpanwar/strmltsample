6a3a98a75941f683524cdcee8e6758b5348651f8
amalpanwar <panwar.amal1995@gmail.com>
2024-07-03 17:19:29 +0100

    Update Strmlit_app.py

--- Strmlit_app.py
+++ Strmlit_app.py
@@ -106,7 +106,7 @@ if df_position is not None:
         width=1000,
         height=600,
         margin=dict(l=50, r=50, b=100, t=100, pad=4),
-        font=dict(size=5)
+        font=dict(size=8)
     )
     fig.update_traces(textposition='top center')
     
