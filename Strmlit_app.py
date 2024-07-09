75c5b2e9205dc96ea72b147eab09963baa491221
amalpanwar <panwar.amal1995@gmail.com>
2024-07-09 21:39:48 +0100

    Update Strmlit_app.py

--- Strmlit_app.py
+++ Strmlit_app.py
@@ -15,7 +15,8 @@ from langchain_core.messages import HumanMessage, AIMessage
 import getpass
 import os
 from langchain import hub
-from langchain_chroma import Chroma
+from langchain.vectorstores import Chroma
+#from langchain_chroma import Chroma
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
