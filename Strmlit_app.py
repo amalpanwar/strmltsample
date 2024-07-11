d3280bc5230d1021c016fb0d076663483cc0fbed
amalpanwar <panwar.amal1995@gmail.com>
2024-07-11 23:30:17 +0100

    Update Strmlit_app.py

--- Strmlit_app.py
+++ Strmlit_app.py
@@ -14,6 +14,7 @@ import plotly.graph_objects as go
 from langchain_core.messages import HumanMessage, AIMessage
 import getpass
 import os
+from dotenv import load_dotenv
 from langchain import hub
 from langchain.vectorstores import Chroma
 #from langchain_chroma import Chroma
