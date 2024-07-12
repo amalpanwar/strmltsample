37d6056b19c775f4ce1b2b14d3879201f4061394
amalpanwar <panwar.amal1995@gmail.com>
2024-07-12 10:18:47 +0100

    Update Strmlit_app.py

--- Strmlit_app.py
+++ Strmlit_app.py
@@ -242,8 +242,8 @@ else:
           ]
          )
 
-       question_answer_chain = create_stuff_documents_chain(llm, prompt)
-       rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+        question_answer_chain = create_stuff_documents_chain(llm, prompt)
+        rag_chain = create_retrieval_chain(retriever, question_answer_chain)
 
 
 
