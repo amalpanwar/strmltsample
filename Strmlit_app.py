c11f0d6a9acf1f2ea70732d431ea97ae13a31ba5
amalpanwar <panwar.amal1995@gmail.com>
2024-07-12 01:44:36 +0100

    Update Strmlit_app.py

--- Strmlit_app.py
+++ Strmlit_app.py
@@ -36,6 +36,7 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.document_loaders import CSVLoader
 import logging
+logging.basicConfig(level=logging.DEBUG)
 
 
 
