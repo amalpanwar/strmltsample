bb608dc630c81ea378975e4329f2772ee0e1034d
amalpanwar <panwar.amal1995@gmail.com>
2024-07-30 17:31:13 +0100

    Update Strmlit_app.py

--- Strmlit_app.py
+++ Strmlit_app.py
@@ -1555,7 +1555,7 @@ elif position == 'FB':
     df_melted = df_filtered2.melt(id_vars='Player', value_vars=['Accurate crosses, %'], var_name='Metric', value_name='Value')
 
     # Create stacked bar chart
-    fig3 = px.bar(df_melted, x='Player', y='Value', color='Metric', orientation='h', title=f'{position} Crossing Skills')
+    fig3 = px.bar(df_melted, x='Value', y='Player', color='Metric', orientation='h', title=f'{position} Crossing Skills')
     st.plotly_chart(fig3)
     
 
