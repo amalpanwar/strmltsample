1e48aa60e0e18819605bc6b5be3004c41925a500
amalpanwar <panwar.amal1995@gmail.com>
2024-08-01 18:06:45 +0100

    Update Strmlit_app.py

--- Strmlit_app.py
+++ Strmlit_app.py
@@ -394,7 +394,7 @@ def create_radar_chart(df, players, id_column, title=None, max_values=None, padd
         ),
         width=1000,  # Increased width for better clarity
         height=300,  # Increased height for better clarity
-        margin=dict(l=110, r=110, t=15, b=0),  # Increased bottom margin to accommodate the legend
+        margin=dict(l=105, r=115, t=18, b=0),  # Increased bottom margin to accommodate the legend
         paper_bgcolor='black',  # Background color
         plot_bgcolor='white',    # Plot area background color
         legend=dict(
