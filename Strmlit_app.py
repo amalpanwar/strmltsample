3ae5d1db3db3b37f523eb214789e5f29c89a263d
amalpanwar <panwar.amal1995@gmail.com>
2024-08-01 17:56:36 +0100

    Update Strmlit_app.py

--- Strmlit_app.py
+++ Strmlit_app.py
@@ -399,9 +399,9 @@ def create_radar_chart(df, players, id_column, title=None, max_values=None, padd
         plot_bgcolor='white',    # Plot area background color
         legend=dict(
             orientation="h",  # Horizontal orientation
-            yanchor="bottom",    # Position the legend above the plot area
+            yanchor="top",    # Position the legend above the plot area
             y=-0.1,           # Vertical position (below the plot area)
-            xanchor="center", # Center the legend horizontally
+            xanchor="left", # Center the legend horizontally
             x=0.5,            # Horizontal position (centered)
             bgcolor='rgba(255, 255, 255, 0)', # Transparent background
             bordercolor='black',
