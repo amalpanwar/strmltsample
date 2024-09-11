bf660d8ae22c3db0887fcc1f257c1c8a24cfd186
amalpanwar <panwar.amal1995@gmail.com>
2024-09-12 00:16:22 +0100

    Update Strmlit_app.py

--- Strmlit_app.py
+++ Strmlit_app.py
@@ -71,6 +71,26 @@ pvt_df_CF = pd.DataFrame(df_CF).set_index('Player')
 pvt_df_GK = pd.DataFrame(df_GK).set_index('Player')
 pvt_df_FB = pd.DataFrame(df_FB).set_index('Player')
 
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+
+def set_background(png_file):
+    bin_str = get_base64(png_file)
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+set_background('./images/background.png')
+
 # Pivot the dataframe
 # pivot_df = df.pivot(index='Player', columns='Attribute', values='Value')
 
